c89fcd92b8ebdbbfe036b12c798caa80fca449a9
barce <jbarcelona@gmail.com>
2011-05-29 15:37:58 -0700

    refactor with before(:each)

--- codebreaker/spec/codebreaker/game_spec.rb
+++ codebreaker/spec/codebreaker/game_spec.rb
@@ -3,15 +3,20 @@ require 'spec_helper'
 module Codebreaker
   describe Game do
     describe "#start" do
+      before(:each) do
+        @output = double('output').as_null_object
+        @game = Game.new(@output)
+      end
+
       it "sends a welcome message" do
-        output = double('output')
-        game   = Game.new(output)
-  
-        output.should_receive(:puts).with('Welcome to Codebreaker!')
-        
-        game.start
+        @output.should_receive(:puts).with('Welcome to Codebreaker!') 
+        @game.start
       end 
-      it "prompts for the first guess"
+
+      it "prompts for the first guess" do
+        @output.should_receive(:puts).with('Enter guess:')
+        @game.start
+      end 
     end
   end
 end
