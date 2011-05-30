df9bae7a0d5f62034e8d8d88679371ca93b79899
barce <jbarcelona@gmail.com>
2011-05-29 18:12:19 -0700

    adding step for guessing

--- codebreaker/features/step_definitions/codebreaker_steps.rb
+++ codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -23,25 +23,21 @@ end
 Given /^I am not yet playing$/ do
 end
 
+Given /^the secret code is "([^"]*)"$/ do |secret|
+  @game = Codebreaker::Game.new(output)
+  @game.start(secret)
+end
+
 When /^I start a new game$/ do
   game = Codebreaker::Game.new(output)
-  game.start
+  game.start('1234')
+end
+
+When /^I guess "([^"]*)"$/ do |guess|
+  @game.guess(guess)
 end
 
 Then /^I should see "([^"]*)"$/ do |message|
   output.messages.should include(message)
-end
-
-Given /^the secret code is "([^"]*)"$/ do |secret| 
-  game = Codebreaker::Game.new(output) 
-  game.start(secret)
-end
-
-When /^I guess "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^the mark should be "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
 end
 
