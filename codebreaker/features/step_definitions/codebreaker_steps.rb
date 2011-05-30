8e48fab47fac404f35e53f12409891a1b31b2295
barce <jbarcelona@gmail.com>
2011-05-29 18:02:23 -0700

    adding scenario involving when a secret is entered

--- codebreaker/features/step_definitions/codebreaker_steps.rb
+++ codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -31,3 +31,17 @@ end
 Then /^I should see "([^"]*)"$/ do |message|
   output.messages.should include(message)
 end
+
+Given /^the secret code is "([^"]*)"$/ do |secret| 
+  game = Codebreaker::Game.new(output) 
+  game.start(secret)
+end
+
+When /^I guess "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the mark should be "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
